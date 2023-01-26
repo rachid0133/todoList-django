5432a2e83ebc65eb30846396612b091402917255
rachid chakor <chakor.rachid.1@gmail.com>
2023-01-25 22:29:48 -0500

    submit tasks to database

--- mytodolist/views.py
+++ mytodolist/views.py
@@ -1,6 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Task
 
 # Create your views here.
 
 def index(request):
-    return render(request, 'index.html')
+    tasks = Task.objects.all()
+    if request.method == 'POST':
+        new_url = Task(title = request.POST['title'])
+        new_url.save()
+        return redirect('/')
+    return render(request, 'index.html', {'task':tasks})
+
+
