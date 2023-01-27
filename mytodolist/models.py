48585ae81d14a02e54e9fb6a7d4696d11a324b0e
rachid chakor <chakor.rachid.1@gmail.com>
2023-01-26 19:40:09 -0500

    added time

--- mytodolist/models.py
+++ mytodolist/models.py
@@ -3,3 +3,7 @@ from django.db import models
 # Create your models here.
 class Task(models.Model):
     title = models.CharField(max_length=1000)
+    time = models.TimeField(blank=True, null=True)
+
+    def __str__(self):
+        return self.title
--- mytodolist/views.py
+++ mytodolist/views.py
@@ -1,12 +1,18 @@
 from django.shortcuts import render, redirect
 from .models import Task
+from datetime import datetime
 
 # Create your views here.
 
 def index(request):
+
     tasks = Task.objects.all()
+    title = request.POST.get('title')
+    times = request.POST.get('time')
+    # my_time = datetime.strptime(times, '%H:%M').time()
+    # print(times)
     if request.method == 'POST':
-        new_url = Task(title = request.POST['title'])
+        new_url = Task(title=title, time=times)
         new_url.save()
         return redirect('/')
     return render(request, 'index.html', {'tasks':tasks})
